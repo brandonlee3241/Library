ebcdd423086532e04bf744f001f0b689d097571b
Brandon Lee <brandonlee3241@hotmail.com>
2023-09-04 23:59:40 -0700

    HTTP Route Added

--- FullStackProject/backend/index.js
+++ FullStackProject/backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get("/",(request,response)=>{
+    console.log(request)
+    return response.status(234).send("Welcome to MERN tutorial");
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
