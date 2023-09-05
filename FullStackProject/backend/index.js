12cc66d15c609a1500699866b7b9033b5be4f6ec
Brandon Lee <brandonlee3241@hotmail.com>
2023-09-05 11:50:20 -0700

    delete book added

--- FullStackProject/backend/index.js
+++ FullStackProject/backend/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
@@ -68,6 +68,68 @@ app.get("/books", async (request,response)=>{
         response.status(500).send({ message:error.message });
     }
 });
+
+// Route for get one book from db by id 
+app.get("/books/:id", async (request,response)=>{
+    try{
+        const { id } = request.params;
+        const book = await Book.findById(id); // find one book by id
+
+        // return the book to the client
+        return response.status(200).json(book);
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(500).send({ message:error.message });
+    }
+});
+
+
+// Route for update a book
+app.put("/books/:id",async (request,response)=>{
+    try{
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ){
+            return response.status(400).send({
+                message: "Required field is missing",
+        });
+        }
+
+        const { id } = request.params;
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if(!result){
+            return response.status(404).json({meessage: "Book not found"});
+        }
+        return response.status(200).send({message: "Book updated successfully"});
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(500).send({ message:error.message });
+    }
+});
+
+// Route for delete a book
+app.delete("/books/:id", async (request,response)=>{
+    try{
+        const {id} = request.params;
+        const result = await Book.findByIdAndDelete(id);
+
+        if(!result){
+            return response.status(404).json({message: "Book not found"});
+        }
+        return response.status(200).send({message: "Book deleted successfully"});
+
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({ message:error.message });
+    }
+});
+    
+  
 mongoose
     .connect(mongoDBURL)
     .then(() => {
