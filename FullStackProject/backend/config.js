fe1d36ec250aeab42531f5a2dd04e167d897b1df
Brandon Lee <brandonlee3241@hotmail.com>
2023-09-05 00:11:59 -0700

    MongoDB setup

--- FullStackProject/backend/config.js
+++ FullStackProject/backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 
+"mongodb+srv://root:root@book-store-mern.dr610ja.mongodb.net/books-collection?retryWrites=true&w=majority"
--- FullStackProject/backend/index.js
+++ FullStackProject/backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -8,6 +9,16 @@ app.get("/",(request,response)=>{
     return response.status(234).send("Welcome to MERN tutorial");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("MongoDB is connected");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
